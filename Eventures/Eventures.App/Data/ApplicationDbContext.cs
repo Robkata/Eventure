b8d65d5fd8149c1f232caac22bfdc62bff33a43c
“Robkata” <r.georgiev2017@pmg-pernik.com>
2022-02-22 11:39:12 +0200

    Create a new project

---
+++ Eventures/Eventures.App/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Eventures.App.Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eventures.App.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<EventuresUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            this.Database.EnsureCreated();
+        }
+    }
+}
